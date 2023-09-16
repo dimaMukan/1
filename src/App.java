e59006141871982a7647ade67b68566081d8fd21
dimaMukan <ds.mukan@gmail.com>
2023-09-16 15:06:16 +0100

    input output

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
+        String inputText=JOptionPane.showInputDialog("Введіть текст:");
+        System.out.println("Введений текст: " + inputText);
+        String inputNumber=JOptionPane.showInputDialog("Введіть число:");
+        double number = Double.parseDouble(inputNumber);
         System.out.println("Hello, World!");
     }
 }
