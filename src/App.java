6965f93ad242c78d4c9883d0749d7972f4efe914
dimaMukan <ds.mukan@gmail.com>
2023-09-16 15:28:43 +0100

    commit_3

--- src/App.java
+++ src/App.java
@@ -1,15 +1,46 @@
+import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
         // String inputText=JOptionPane.showInputDialog("Введіть текст:");
         // System.out.println("Введений текст: " + inputText);
         // String inputNumber=JOptionPane.showInputDialog("Введіть число:");
         // double number = Double.parseDouble(inputNumber);
         // System.out.println("Введений текст: " + inputNumber);
+
         System.out.println("Hello, World!");
-        JOptionPane.showMessageDialog(null,"Це повідомлення!");
-        JOptionPane.showMessageDialog(null,"Це інформаційне повідомлення.", "Інформація", JOptionPane.INFORMATION_MESSAGE);
-        JOptionPane.showMessageDialog(null,"Сталася помилка!", "Помилка",JOptionPane.ERROR_MESSAGE);
+        
+        // JOptionPane.showMessageDialog(null,"Це повідомлення!");
+        // JOptionPane.showMessageDialog(null,"Це інформаційне повідомлення.", "Інформація", JOptionPane.INFORMATION_MESSAGE);
+        // JOptionPane.showMessageDialog(null,"Сталася помилка!", "Помилка",JOptionPane.ERROR_MESSAGE);
+    
+
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Введіть ваше ім'я: ");
+        // String name = scanner.nextLine();
+        // System.out.print("Введіть ваш вік: ");
+        // int age = scanner.nextInt();
+        // System.out.println("Привіт, " + name + "! Тобі " + age + " років.");
+        // scanner.close();
+
+        // String message = "Це приклад виведення тексту в консоль.";
+        // System.out.println(message);
+        // int number = 42;
+        // System.out.println("Число: " + number);
+        // double pi = 3.14159265359;
+        // // Виведення з форматуванням
+        // System.out.printf("Значення числа pi: %.2f%n", pi);
+
+
+        // String inputText = JOptionPane.showInputDialog("Введіть ваше ім'я:");
+        // if(inputText != null && !inputText.isEmpty()){
+        // System.out.println("Ваше ім'я: " + inputText);
+        // }else{
+        // System.out.println("Ви не ввели ім'я.");
+        // }
+
     }
 }
