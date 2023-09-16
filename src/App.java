c67d28a79be5cff43ad830e80b847e85b7c646e2
dimaMukan <ds.mukan@gmail.com>
2023-09-16 15:12:29 +0100

    showMessageDialog

--- src/App.java
+++ src/App.java
@@ -2,10 +2,14 @@ import javax.swing.JOptionPane;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String inputText=JOptionPane.showInputDialog("Введіть текст:");
-        System.out.println("Введений текст: " + inputText);
-        String inputNumber=JOptionPane.showInputDialog("Введіть число:");
-        double number = Double.parseDouble(inputNumber);
+        // String inputText=JOptionPane.showInputDialog("Введіть текст:");
+        // System.out.println("Введений текст: " + inputText);
+        // String inputNumber=JOptionPane.showInputDialog("Введіть число:");
+        // double number = Double.parseDouble(inputNumber);
+        // System.out.println("Введений текст: " + inputNumber);
         System.out.println("Hello, World!");
+        JOptionPane.showMessageDialog(null,"Це повідомлення!");
+        JOptionPane.showMessageDialog(null,"Це інформаційне повідомлення.", "Інформація", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null,"Сталася помилка!", "Помилка",JOptionPane.ERROR_MESSAGE);
     }
 }
